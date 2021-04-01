78513b8ec8fe920de5b8ad92613e9f1d54542bf9
Roshanak Agahi <rosh.agahi@gmail.com>
2021-04-01 05:31:35 +0000

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,28 @@
+  
+import React, { Component } from 'react';
+    
+export default class Cell extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      color: props.value,
+    };
+  }
+    
+  mouseClick = () => {
+    const newColor = "#333";
+    this.setState({
+      color: newColor
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className="cell"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.mouseClick}
+      ></div>
+    );
+  }
+}
