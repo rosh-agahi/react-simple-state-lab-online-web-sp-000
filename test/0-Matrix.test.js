d17280b3b7b65f585cb79be8bcf64b5a951c49c7
Roshanak Agahi <rosh.agahi@gmail.com>
2021-04-01 05:26:50 +0000

    Automatically backed up by Learn

--- test/0-Matrix.test.js
+++ test/0-Matrix.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { configure, shallow } from 'enzyme';
 import { expect } from 'chai';
-// import sinon from 'sinon';
+import sinon from 'sinon';
 
 import Adapter from 'enzyme-adapter-react-16';
 
